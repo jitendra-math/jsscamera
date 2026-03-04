a902a8c1f2039c4df9b5957b3260323b5b0fbba0
Jitendra Singh Nimod <i.jitendra.singh0@gmail.com>
2026-03-04 19:43:50 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Tailwind CSS aur doosre preprocessors ko handle karne ke liye
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto Vercel deployment ke liye best hai
 		adapter: adapter()
 	}
 };
