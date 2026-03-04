8dd6095423cc07675f8a122179e7b033e7e88d34
Jitendra Singh Nimod <i.jitendra.singh0@gmail.com>
2026-03-04 21:04:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
+// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	worker: {
-		/* FIX: 'es' ki jagah 'iife' use kar rahe hain. 
-		   Yeh zaroori hai taaki imageProcessor.js mein 
-		   importScripts() bina kisi error ke kaam kare.
-		*/
-		format: 'iife' 
+		format: 'es'   // was 'iife'
 	}
 });
